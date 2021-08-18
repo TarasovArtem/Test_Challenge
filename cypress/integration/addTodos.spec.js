88f767e7fe9e2cf7ca8e9eb6af6963a74ff7d0a9
TarasovArtem <Hans8883@yandex.ru>
2021-08-18 21:37:03 +0200

    created new project

---
+++ cypress/integration/addTodos.spec.js
@@ -0,0 +1,14 @@
+/// <reference types="Cypress" />
+
+
+
+describe('Test of checkboxes', () => {
+
+    beforeEach(() => {
+        cy.visit('/');
+    });
+
+    it('should check the checkboxes', () => {
+        
+    });
+});
