9beb1b0792529308a2bbc8c15d1efa26f18b8700
TarasovArtem <Hans8883@yandex.ru>
2021-08-21 17:58:53 +0200

    fix plugin file

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,5 +1,11 @@
 const cypressTypeScriptPreprocessor = require('./cy-ts-preprocessor')
+const webpackPreprocessor = require('../../')
+
+/**
+ * @type {Cypress.PluginConfig}
+ */
 
 module.exports = on => {
-  on('file:preprocessor', cypressTypeScriptPreprocessor)
+  on('file:preprocessor', cypressTypeScriptPreprocessor, webpackPreprocessor())
 }
+
